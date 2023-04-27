b7dce31c332c76011c876504ffc1666822d1f9e6
K-Oussama <oussama_korchi@um5.ac.ma>
2023-04-27 11:34:03 -0600

    apos

--- src/components/thanks.jsx
+++ src/components/thanks.jsx
@@ -6,9 +6,10 @@ const Thanks = ({ setCardData, setValidate }) => {
 
     return (
         <div className="cardThanks text-center text-slate-900 max-w-lg">
+          {/* Later replace img tag with next/image*/} 
         <img src="/icon-complete.svg" className="inline mb-7" alt="complete" />
         <p className="text-lg uppercase mb-0">Thank you!</p>
-        <p className="opacity-80 text-xs mt-3 mb-7">We've added your card details</p>
+        <p className="opacity-80 text-xs mt-3 mb-7">We&apos;ve added your card details</p>
         <button className="w-2/3 px-5 py-4 cursor-pointer bg-slate-900 text-white border rounded-lg text-base" onClick={reset}>Continue</button>
       </div>
     )
